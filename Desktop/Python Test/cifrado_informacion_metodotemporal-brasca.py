07315c5c2bb11237c8b5841d5556141009ac6b4d
Ignacio <warkanlock@gmail.com>
2018-09-15 21:48:40 -0300

    First Commit

---
+++ Desktop/Python Test/cifrado_informacion_metodotemporal-brasca.py
@@ -0,0 +1,89 @@
+import time
+import random 
+import matplotlib.pyplot as plt
+import timeit
+
+start = time.time()
+
+#TESTING SEED
+import time
+year, month, day, hour, minute, secunde = time.strftime("%Y,%m,%d,%I,%M,%S").split(',')
+
+#GENERAR LA SEED Y LA KEY
+#localtime = time.asctime( time.localtime(time.time()) )
+#print "seed form: ", localtime
+#seed = input("Enter the seed: ")
+
+seed = (int(year)/int(month)) + (int(month) + int(day) + int(hour) + int(minute)) ** int(secunde)
+peso = 60 #peso
+print "Semilla Generada por tiempo / Seed generate by time: ", seed
+
+#INGRESA EL CODIGO DE ENTRADA
+code = input("Ingresar el codigo a cifrar / Input the code to encode: ")
+
+random.seed(seed);
+
+key = random.random() #llave primaria
+key_constelacion = ((random.uniform(peso, seed))/(seed/key)) #llave_constelacion
+
+def generate_random_key_list(key, key_constelacion):
+	secret_key = []
+	for i in range(0,10):
+		#data = (i)/(key)*((i-1)**key**time.time()) + peso 
+		data = (i)/(key)*((key-1)**key_constelacion**time.time()) + peso 
+		if(data>128):
+			data = data/4
+		secret_key.append(data)
+		#secret_key[i-1] = secret_key.append(data)
+		#print secret_key[i]
+	return secret_key
+
+def generate_word(code):
+	word_complete = ""
+	for i in range(0,len(code)):
+		word_complete = word_complete + (code[i] + '.')
+	return word_complete.split('.')
+
+def encode(word, list_keys):
+	for i in range(0, len(word)-1):
+		word[i] = repr(list_keys[random.randint(0,9)])
+
+	word_encode = word
+
+	return word_encode #es una lista, no un string
+
+def decode(word_encode):
+	output_word = []
+	for i in range(0, len(word_encode)-1):
+		output_word.append(str(unichr(int(float(word_encode[i])))))
+		#letra_convertida.append(str(unichr(int(word_encode[i]))))
+	return output_word
+
+#VALORES DE LA RANDOM KEY
+testing_list = generate_random_key_list(key, key_constelacion)
+print "Claves senuelo: \n", testing_list
+
+#GENERAR EL CODIGO A ENCRIPTAR#
+#code = input("Input the code to encode: ")
+#code = "Nano"
+
+#RECIBIR LA PALABRA 
+word_complete = generate_word(code)
+
+#ESCRIBIR LA PALABRA ENCRIPTADA
+print "Informacion Real: \n", word_complete
+
+#ABSTRAER
+word_encode = encode(word_complete, testing_list)
+print "Informacion Cifrada: \n", word_encode
+
+#CONVERTIR A TEXTO
+print "Palabra Codigo: \n", decode(word_encode)
+
+word_final = ''.join(decode(word_encode))
+print word_final 
+
+end = time.time()
+
+print "Tiempo requerido:" 
+print(end-start)
